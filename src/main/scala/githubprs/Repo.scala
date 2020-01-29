0b72c39645ce3fbcc84320fdcb76835842cef15d
Kelvin Chappell <kelvin.chappell@theguardian.com>
2020-01-29 11:33:53 +0000

    Fetch repo language

--- src/main/scala/githubprs/Repo.scala
+++ src/main/scala/githubprs/Repo.scala
@@ -4,7 +4,7 @@ import io.circe.Decoder
 import io.circe.generic.extras.Configuration
 import io.circe.generic.extras.semiauto._
 
-case class Repo(fullName: String)
+case class Repo(fullName: String, language: Option[String])
 
 object Repo {
   implicit val config: Configuration  = Configuration.default.withSnakeCaseMemberNames.withDefaults
